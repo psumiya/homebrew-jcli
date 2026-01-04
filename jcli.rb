862f73ef07c1b5314f1cdf6463a7b400fca72637
Sumiya <sumiya.p@gmail.com>
2026-01-03 18:42:18 -0800

    Add sha for Mac

--- jcli.rb
+++ jcli.rb
@@ -8,7 +8,7 @@ class Jcli < Formula
     sha256 "132de7b62340934215b84fa935b9258edd4d01fe32b105d59f04811fa9d655ee"
   else
     url "https://github.com/psumiya/jcli/releases/download/v0.0.1-beta/jcli-0.0.1-beta-osx-aarch64.tar.gz"
-    sha256 "REPLACE_WITH_SHA256_OF_ARM64_TARBALL"
+    sha256 "sha256:132de7b62340934215b84fa935b9258edd4d01fe32b105d59f04811fa9d655ee"
   end
 
   def install
