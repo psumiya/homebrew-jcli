12fcd223acd15a073a922499c20896e35a302ea1
Sumiya <sumiya.p@gmail.com>
2026-01-04 17:51:47 -0800

    Update jcli version to 0.0.3-beta

--- jcli.rb
+++ jcli.rb
@@ -3,8 +3,8 @@ class Jcli < Formula
   homepage "https://github.com/psumiya/jcli"
   version "0.0.2-beta"
 
-  url "https://github.com/psumiya/jcli/releases/download/v0.0.2-beta/jcli-0.0.2-beta-osx-x86_64.tar.gz"
-  sha256 "2aeb09de61e915fcba85136b7f039679eff4e64985f8f5cc19f540cd03822840"
+  url "https://github.com/psumiya/jcli/releases/download/v0.0.3-beta/jcli-0.0.3-beta-osx-x86_64.tar.gz"
+  sha256 "bedf23ef4d862a72dd8590c08f602faec4c0bca63311d47218d5328993bc3caf"
 
   def install
     bin.install "jcli"
